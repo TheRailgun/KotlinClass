0a29005a2a3be6be74978f52ad6a2b451e9ff272
AlanTuring <andrewjrussell95@gmail.com>
2022-02-24 18:22:23 -0500

    Initial Commit

---
+++ Class2.kt
@@ -0,0 +1,18 @@
+import kotlin.math.*
+
+fun main(args : Array<String>) {
+
+    println(PI)
+
+    fun division(a:Int, b:Int): Double{
+        if(b==0){
+            throw IllegalArgumentException(
+                "Months cant be 0"
+            ) 
+        } else
+         return a.toDouble()/b.toDouble()
+    }
+
+    println(division(3,2))
+    //println(division(3,0))//this throws the above exception
+}
---
+++ MeansOfTransportation.kt
@@ -0,0 +1,9 @@
+class MeansOfTransportation{
+
+    var numberOfPassengers :Int = 0
+
+    fun addPass(change: Int){
+        numberOfPassengers += change;
+    }
+
+}
